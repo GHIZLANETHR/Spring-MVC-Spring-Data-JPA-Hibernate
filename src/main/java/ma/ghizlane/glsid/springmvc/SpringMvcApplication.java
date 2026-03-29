6189044acb80618ebea718ca85ece5d800a451d7
Ghizlanethr <touahrighizlane98@gmail.com>
2026-03-29 12:53:03 +0100

    feat: ajout de deux nouveaux produits (Printer, Smart Phone) et affichage de tous les produits dans la console au démarrage

--- src/main/java/ma/ghizlane/glsid/springmvc/SpringMvcApplication.java
+++ src/main/java/ma/ghizlane/glsid/springmvc/SpringMvcApplication.java
@@ -1,7 +1,11 @@
 package ma.ghizlane.glsid.springmvc;
 
+import ma.ghizlane.glsid.springmvc.entities.Product;
+import ma.ghizlane.glsid.springmvc.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMvcApplication {
@@ -10,4 +14,27 @@ public class SpringMvcApplication {
         SpringApplication.run(SpringMvcApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(Product.builder()
+                    .name("Computer")
+                    .price(5400)
+                    .quantity(12)
+                    .build());
+            productRepository.save(Product.builder()
+                    .name("Printer")
+                    .price(1200)
+                    .quantity(11)
+                    .build());
+            productRepository.save(Product.builder()
+                    .name("Smartphone")
+                    .price(1200)
+                    .quantity(33)
+                    .build());
+            productRepository.findAll().forEach(p -> {
+                System.out.println(p.toString());
+            });
+        };
+    }
 }
