6fac2c18003f24b562fdfaf97fcb952dfbbe6697
Ghizlanethr <touahrighizlane98@gmail.com>
2026-04-05 14:33:01 +0100

    ajouter la suppression d'un produit

--- src/main/java/ma/ghizlane/glsid/springmvc/web/ProductController.java
+++ src/main/java/ma/ghizlane/glsid/springmvc/web/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,5 +20,10 @@ public class ProductController {
         System.out.println(products);
         model.addAttribute("productList",products);
         return "products";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
     }
 }
